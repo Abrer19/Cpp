ad6a283ed56f3bad34223ba4a0752fa98c93f435
Abrar Ahmad Chy <abrarahmadchowdhury2002@gmail.com>
2025-11-25 22:52:50 +0600

    Add files via upload

--- F. Eating Candies.cpp
+++ F. Eating Candies.cpp
@@ -8,28 +8,34 @@ vector<ll>v(n);
 for(int i=0;i<n;i++){
     cin>>v[i];
 }
-int i=0,j=n-1;
+
+vector<ll>suffix(n);
+ll sum=0;
+map<ll,ll>index;
+for(int i=n-1;i>=0;i--){
+    sum+=v[i];
+    index[sum]=i;
+    suffix[i]=sum;
+}
 ll ans=0;
-ll sum1=0,sum2=0;
-while(i<j){
-    sum1+=v[i];
-    sum2+=v[j];
-    i++;
-    j--;
- if(sum1==sum2){
-    ans=max(ans,(i+(n-j-1)));
- }
+ll prefix_sum=0;
+for(int i=0;i<n-1;i++){
+    index.erase(suffix[i]);
+    prefix_sum+=v[i];
+    if(index.find(prefix_sum)!=index.end()){
+        ans=max(ans,(i+1)+(n-index[prefix_sum]));
+    }
 }
 
+
 cout<<ans<<endl;
+
+
 }
 int main(){
 ll t;
 cin>>t;
 while(t--){
-solve();
+    solve();
 }
-
-
-
 }
